0b9d19ff97c36c3b8d7a73d9d84836e87155c688
JulienGrade <gradejulien@gmail.com>
2024-02-29 14:43:20 +0100

    Add pipeline

---
+++ tests/Controller/HomepageControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomepageControllerTest extends WebTestCase
+{
+    private function requestHomepage(): Crawler
+    {
+        // Cette méthode nous donne un client
+        $client = static::createClient();
+        // On lance une requête sur l'url / depuis notre client
+        return $client->request('GET', '/');
+    }
+
+    /**
+     * Permet de tester que l'on reçoit un code 200 lorsque l'on est sur / soit l'accueil du site
+     * @return void
+     */
+    public function testHomepage(): void
+    {
+        $this->requestHomepage();
+        self::assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+}
